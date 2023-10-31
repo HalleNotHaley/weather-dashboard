06aeda1af889eaec93127f5737ba170ece149075
HalleNotHaley <hallepodolanko@gmail.com>
2023-10-30 21:19:31 -0500

    updated HTML

---
+++ script.js
@@ -0,0 +1,6 @@
+var APIKey = "8de1e268d7481fb2c48388f72229113e";
+var temp;
+var wind;
+var humidity;
+
+<script src="https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=8de1e268d7481fb2c48388f72229113e"></script>
